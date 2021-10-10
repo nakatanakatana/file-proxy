0874ad68e1c22f9e047906d6d82d63440f83d31b
tanaka <nakatanakatana@gmail.com>
2021-10-10 17:47:32 +0900

    refactoring

--- middleware_gcs.go
+++ middleware_gcs.go
@@ -3,6 +3,8 @@ package gcsproxy
 import (
 	"context"
 	"io"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 
@@ -44,4 +46,18 @@ func DownloadGCSObject(dir, filePath string, bucket *storage.BucketHandle) error
 	}
 
 	return nil
+}
+
+func GetGCSFile(dir string, bucket *storage.BucketHandle, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if path != "" {
+			filePath := path[1:]
+			err := DownloadGCSObject(dir, filePath, bucket)
+			if err != nil {
+				log.Println("DownloadObjectError", dir, filePath, err)
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
 }
---
+++ middleware_helper.go
@@ -0,0 +1,11 @@
+package gcsproxy
+
+import (
+	"net/http"
+)
+
+
+func CreateFileServer(rootPath string) http.Handler {
+	return http.FileServer(http.Dir(rootPath))
+}
+
